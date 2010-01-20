5ff63e5d78094cf8adf6c959d6a9eb5573b2838c
Michael Vogt <michael.vogt@ubuntu.com>
2010-01-20 10:40:26 +0100

    tests/test_acl.sh: add test counts

--- tests/test_acl.sh
+++ tests/test_acl.sh
@@ -1,18 +1,25 @@
-#/bin/sh
+#!/bin/sh
 
 CURL="curl -s -I"
+count=0
+failure=0
 
-function assert_ok() {
+assert_ok() {
     if ! $CURL $1 | grep -q "HTTP/1.0 200 OK"; then
 	echo "ASSERT FAILURE for $@, assumed 200 OK"
+	failure=$(( $failure + 1 ))
     fi
+    count=$(( $count + 1 ))
 }
 
-function assert_forbidden() {
+assert_forbidden() {
     if ! $CURL $1 | grep -q "HTTP/1.0 403 Forbidden"; then
 	echo "ASSERT FAILURE for $@, assumed 403 Forbidden"
+	failure=$(( $failure + 1 ))
     fi
+    count=$(( $count + 1 ))
 }
+
 
 http_proxy=http://localhost:8000/
 echo "Starting tests for $http_proxy"
@@ -21,7 +28,9 @@ assert_ok "http://archive.ubuntu.com"
 assert_ok "http://de.archive.ubuntu.com/ubuntu/"
 assert_ok "http://changelogs.ubuntu.com/meta-release"
 
+assert_forbidden "http://wiki.ubuntu.com"
+assert_forbidden "http://www.ubuntu.com"
 assert_forbidden "http://example.com"
 assert_forbidden "http://example.com:21"
 
-echo "Test finished"
+echo "Test finished, tests failed: $failure, tests run: $count"
